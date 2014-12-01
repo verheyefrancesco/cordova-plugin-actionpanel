a71e5558ea305ebdfe4dbd178d1f500ac6b7bc4d
Francesco Verheye <francesco.verheye@bitmunks.com>
2014-12-01 15:03:01 +0100

    js updated

---
+++ www/android/ActionPanelPlugin.js
@@ -0,0 +1,47 @@
+
+/**
+ * Constructor
+ */
+function ActionPanelPlugin() {
+  //this._callback;
+}
+
+/**
+ * show - true to show the ad, false to hide the ad
+ */
+ActionPanelPlugin.prototype.show = function(options, cb) {
+  
+	var defaults = {
+        title : 'Actions'
+    };
+
+	for (var key in defaults) {
+		if (typeof options[key] !== "undefined") {
+			defaults[key] = options[key];
+		}
+	}
+
+	//this._callback = cb;
+
+	var callback = function(message) {
+		cb(message);
+	}
+  
+	cordova.exec(callback, 
+		null, 
+		"ActionPanelPlugin", 
+		defaults.title,
+		[defaults]
+	);
+};
+
+var actionPanel = new ActionPanelPlugin();
+module.exports = actionPanel;
+
+// Make plugin work under window.plugins
+if (!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.actionPanel) {
+    window.plugins.actionPanel = actionPanel;
+}
